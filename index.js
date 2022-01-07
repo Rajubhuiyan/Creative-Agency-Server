e1bddf6953fc066a7fd548fbd6011e70ce4459ba
rajubhuiyan <rajubhuiyaann@gmail.com>
2022-01-07 15:20:53 +0600

    bug fixed

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const fileUpload = require('express-fileupload');
 const app = express()
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 app.use(fileUpload());
 require('dotenv').config();
 
